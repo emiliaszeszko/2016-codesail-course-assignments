6895d7778df8fdaa2554caf172f3ecdd55f21844
Emilia Szeszko <emilia.szeszko@aya.yale.edu>
2016-06-05 16:03:04 -0400

    updated fractions wip and yelp ica

--- week-2/emilia-class-fractions.rb
+++ week-2/emilia-class-fractions.rb
@@ -4,19 +4,25 @@ class Fractions
     @numerator = numerator
     @denominator = denominator
   end
+
   def to_decimal
-    if denominator != 0
-    puts "numerator".to_f / "denominator".to_f
-  else
-    puts "Error, your denominator cannot equal zero."
+    if @denominator != 0
+      puts @numerator.to_f / @denominator.to_f
+    else
+      puts "Error, your denominator cannot equal zero."
+    end
   end
+
   def to_fraction
-    puts "numerator.to_s/denominator.to_s"
+    puts @numerator.to_s + "/" + @denominator.to_s
   end
+
   def simplify
-    puts "(numerator.to_f/denominator.to_f).to_s"
+    puts @numerator.to_f / @denominator.to_f
   end
+
   def multiplication(numerator, denominator)
-    puts Float
+    puts "Float"
   end
+
 end
